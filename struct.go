84f36fa74725833da2cd2c3e273dc54764159ce7
kenya6565 <ken.australia0605@gmail.com>
2022-12-25 22:03:22 +0900

    fix struct.go

--- struct.go
+++ struct.go
@@ -16,6 +16,10 @@ type person2 struct {
 // person2のポインタが入ったスライス
 type person3 []*person2
 
+type person4 []person2
+
+type person5 person2
+
 func newPerson(name string) *person {
 
     p := person{name: name}
@@ -35,6 +39,20 @@ func renamePerson(p []*person2) {
 
 }
 
+func renamePersonWithoutPtr(p []person2) {
+	fmt.Println(p)
+
+	p[0].age = 200
+
+}
+
+func renamePersonSingle(p person5) person5 {
+
+	p.age = 1000
+	return p
+
+}
+
 func struct_main() {
 	// {Bob 20}
     // fmt.Println(person{"Bob", 20})
@@ -44,29 +62,50 @@ func struct_main() {
 
 		//person2のポインタであってname, ageのポインタではないのでポインタは1つしかない
 		personPtr := person3{ &person2{name: "Bob", age: 40}}
+
 		// [0xc000010030]
 		fmt.Println(&personPtr)
 
-		// &{Bob 100}
-		renamePerson(personPtr)
+		// // &{Bob 100}
+		// renamePerson(personPtr)
 
-		// {Bob 100}
-		fmt.Println(*personPtr[0])
+		// // {Bob 100}
+		// fmt.Println(*personPtr[0])
+
+
+
+		personWithoutPtr := person4{ person2{ name: "Joy", age: 55}}
+
+		renamePersonWithoutPtr(personWithoutPtr)
+
+		fmt.Println(personWithoutPtr)
+
+		peronSingle := person5{ name: "Bob", age:3 }
+		fmt.Println(peronSingle)
+		var val person5
+		val = renamePersonSingle(peronSingle)
+
+		fmt.Println(val)
+
+
+		
+
+		
 
     // fmt.Println(&person2{name: "Ann", age: 40})
 
-		// person1 := &person{name: "Ann", age: 40}
-		// fmt.Println(person1.age)
+		person1 := &person{name: "Ann", age: 40}
+		fmt.Println(person1)
 
 
-    // fmt.Println(newPerson("Jon"))
+    fmt.Println(newPerson("Jon"))
 
-    // s := person{name: "Sean", age: 50}
-    // fmt.Println(s.name)
+    s := person{name: "Sean", age: 50}
+    fmt.Println(s.name)
 
-    // sp := &s
-    // fmt.Println(sp.age)
+    sp := &s
+    fmt.Println(sp.age)
 
-    // sp.age = 51
-    // fmt.Println(sp.age)
+    sp.age = 51
+    fmt.Println(sp.age)
 }
